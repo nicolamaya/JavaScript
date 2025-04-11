9138c89c80745a123acce541005cf2dd3d37e731
nicolamaya <t6104557@gmai.com>
2025-04-11 12:34:30 -0500

    subiendo tablas

---
+++ tablas_de_verdad.js
@@ -0,0 +1,30 @@
+console.log('Tablas de Verdad')
+
+console.log('Conjunción')
+console.log('True + True =', true && true)
+console.log('True + False =', true && false)
+console.log('False + True =', false && true)
+console.log('False + False =', false && false)
+
+console.log('Disyunción')
+console.log('True + True =', true || true)
+console.log('True + False =', true || false)
+console.log('False + True =', false || true)
+console.log('False + False =', false || false)
+
+console.log('Negación')
+console.log('-True =', !true)
+console.log('-False =', !false)
+
+console.log('Condicional o Implicación')
+console.log('True + True =', true && true)
+console.log('True + False =', true && false)
+console.log('False + True =', false || true)
+console.log('False + False=', false || true)
+
+
+console.log('Bicondicional o Doble Implicación')
+console.log('True + True =', true && true)
+console.log('True + False =', true && false)
+console.log('False + True =', false && false)
+console.log('False + False=', false || true)
